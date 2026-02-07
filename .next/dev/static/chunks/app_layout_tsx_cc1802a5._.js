045ff7f1716acd52db96c28a48d2ad00bcb06f38
Tom <twdickman@gmail.com>
2026-02-08 08:22:23 +1100

    fix: Remove next cache files from git

--- .next/dev/static/chunks/app_layout_tsx_cc1802a5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__51d697a8._.css",
-  "static/chunks/_56bfe1a4._.js"
-],
-    source: "dynamic"
-});
